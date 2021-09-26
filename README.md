5507f63ba03cbd2c549b9ff1043e44d489c57d4f
Vaughan Kitchen <vaughan.kitchen@tracplus.com>
2021-09-26 22:30:08 +1300

    Windi 🌬️

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Take-home exercise for [Streem](https://www.streem.com.au/)
 
+## To run
+
+`cd backend; ./bin/rails server`
+
+## Notes
+
+* Had to use Vite cause Webpack doesn't like the exclamation marks
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -31,6 +31,16 @@ const count = ref(0)
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+
+  <button
+    bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+    text="sm white"
+    font="mono light"
+    p="y-2 x-4"
+    border="2 rounded blue-200"
+  >
+    Button
+  </button>
 </template>
 
 <style scoped>
